d7e8d681761e33469f26087595746ec6e10ddc35
aaog2 <aungaungoogame2@gmail.com>
2024-08-08 18:31:01 +0630

    feat: testing api

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { CarouselDefault } from "./components/BannerCarousel";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen p-5">
+      <CarouselDefault />
+    </div>
+  );
+}
---
+++ src/components/BannerCarousel.jsx
@@ -0,0 +1,36 @@
+import { Carousel } from "@material-tailwind/react";
+import axios from "axios";
+import { useEffect } from "react";
+
+export function CarouselDefault() {
+  const getBanner = async () => {
+    const res = await axios.get(
+      "https://dev-changan-automobile.onrender.com/api/v1/banners/public"
+    );
+    console.log(res);
+  };
+
+  useEffect(() => {
+    getBanner();
+  }, []);
+
+  return (
+    <Carousel className="rounded-xl">
+      <img
+        src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+        alt="image 1"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+        alt="image 2"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+    </Carousel>
+  );
+}
